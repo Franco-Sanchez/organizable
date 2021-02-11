d9f58d1331ab244b057cea6233ede77eca8c84b9
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-02-11 12:44:06 -0500

    Logout done and navigation bar without funcionality

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-// import { BoardServices } from "./assets/scripts/services/board_services.js.js";
-// import { UserServices } from "./assets/scripts/services/user_services.js.js";
-// import { SignUp } from "./assets/scripts/signup.js";
-// import { STORE } from './assets/scripts/store.js';
+// import { BoardServices } from "./assets/scripts/services/board_services.js";
+import { Layout } from "./assets/scripts/layout.js";
+import { UserServices } from "./assets/scripts/services/user_services.js";
+import { SignUp } from "./assets/scripts/signup.js";
+import { STORE } from './assets/scripts/store.js';
 
-// function init() {
-//   const signUp = new SignUp('.js-content');
-//   signUp.render();
-// }
+async function init() {
+  const signUp = new SignUp('.js-content');
+  const layout = new Layout('.js-content');
+  if (sessionStorage.getItem('token')) {
+      try {
+        const userServices = new UserServices();
+        // const boardServices = new BoardServices();
+        STORE.user = await userServices.show(sessionStorage.getItem('id'));
+        // STORE.boards = await boardServices.boards();
+        layout.render();
+      } catch (e) {
+        if (e.message === 'Access denied') {
+          sessionStorage.removeItem('token');
+          signUp.render();
+        }
+      }
+  } else {
+    signUp.render();
+  } 
+}
 
-// init()
-
-// async function init() {
-//   const login = new Login('.js-content')
-//   if (sessionStorage.getItem('token')) {
-//       try {
-//         const userServices = new UserServices();
-//         const boardServices = new BoardServices();
-//         STORE.user = await userServices.show(); // falta pasar el id
-//         STORE.boards = await boardServices.boards();
-//         console.log('Acceso habilitado')
-//       } catch (e) {
-//         if (e.message === 'Access denied') {
-//           sessionStorage.removeItem('token');
-//           login.render();
-//         }
-//       }
-//   } else {
-//     login.render();
-//   } 
-// }
-
-// init();
+init();
