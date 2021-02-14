74e31d9057d67ba2f3969e89b6ab1232d141cc98
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-02-14 01:31:01 -0500

    A user can create a card in a specific list

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -2,6 +2,7 @@ import { Card } from "./card.js";
 import { ListServices } from "./services/list_services.js";
 import { STORE } from "./store.js";
 import { CurrentBoard } from "./current_board.js";
+import { CardServices } from "./services/card_services.js";
 
 export function List(parentSelector, dataList) {
   this.parentSelector = parentSelector;
@@ -26,6 +27,9 @@ List.prototype.addEventListeners = function () {
     card.addEventListeners();
   });
   this.deleteList();
+  this.openFormCard();
+  this.closeFormCard();
+  this.addFormCard();
 };
 
 List.prototype.generateCards = function (parentSelector) {
@@ -34,6 +38,19 @@ List.prototype.generateCards = function (parentSelector) {
     return new Card(parentSelector, card);
   });
   container.innerHTML = cards.join("");
+  container.innerHTML += `
+    <div class="js-open-form-card-${this.data.listId} card__open-form">
+      <img src="./assets/images/open_form_card.svg" alt="open-form-card">
+      <p>Add another card</p>
+    </div>
+    <form class="js-create-card-${this.data.listId} list__form">
+      <input type="text" placeholder="Enter a title for this card..." name="name" required>
+      <div>
+        <button type="submit">Add Card</button>
+        <img class="js-close-form-card-${this.data.listId}" src="./assets/images/close_form.svg" alt="close-form">
+      </div>
+    </form>
+  `;
   return cards;
 };
 
@@ -44,14 +61,76 @@ List.prototype.deleteList = function () {
   deleteList.addEventListener("click", async (e) => {
     try {
       const listServices = new ListServices();
-      await listServices.delete(
-        STORE.currentBoard.id,
-        this.data.listId
-      );
+      await listServices.delete(STORE.currentBoard.id, this.data.listId);
       STORE.currentBoard.lists = STORE.currentBoard.lists.filter(
         (list) => list.listId !== this.data.listId
       );
       const currentBoard = new CurrentBoard(".js-content");
+      currentBoard.render();
+    } catch (e) {
+      console.log(e);
+      alert(e.message);
+    }
+  });
+};
+
+List.prototype.openFormCard = function () {
+  const openForm = this.parentElement.querySelector(
+    `.js-open-form-card-${this.data.listId}`
+  );
+  const form = this.parentElement.querySelector(
+    `.js-create-card-${this.data.listId}`
+  );
+  openForm.addEventListener("click", () => {
+    openForm.style.display = "none";
+    form.style.display = "block";
+  });
+};
+
+List.prototype.closeFormCard = function () {
+  const openForm = this.parentElement.querySelector(
+    `.js-open-form-card-${this.data.listId}`
+  );
+  const form = this.parentElement.querySelector(
+    `.js-create-card-${this.data.listId}`
+  );
+  const closeForm = this.parentElement.querySelector(
+    `.js-close-form-card-${this.data.listId}`
+  );
+  closeForm.addEventListener("click", () => {
+    openForm.style.display = "flex";
+    form.style.display = "none";
+    form.reset();
+  });
+};
+
+List.prototype.addFormCard = function () {
+  const form = this.parentElement.querySelector(
+    `.js-create-card-${this.data.listId}`
+  );
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const name = form.name.value;
+      const cardServices = new CardServices();
+      const data = await cardServices.create(this.data.listId, name, null);
+      const newCard = {
+        cardId: data.id,
+        name: data.name,
+        desc: data.desc,
+        pos: data.pos,
+        closed: data.closed,
+        labels: data.labels,
+        checkItems: 0,
+        completedCheckItems: 0,
+      };
+      STORE.currentBoard.lists = STORE.currentBoard.lists.map(list => {
+        if(list.listId === this.data.listId) {
+          return { ...list, cards: [...list.cards, newCard] }
+        }
+        return list;
+      })
+      const currentBoard = new CurrentBoard('.js-content')
       currentBoard.render();
     } catch (e) {
       console.log(e);
